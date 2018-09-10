a384149e1b185cc9a660b2b7d6309fe1a448a036
姬阔东 <jikuodong@froeasy.net>
2018-09-10 13:37:57 +0800

    添加flyio请求配置

--- mpvue/src/main.js
+++ mpvue/src/main.js
@@ -7,5 +7,38 @@ App.mpType = 'app'
 
 // 挂载store
 Vue.prototype.$store = store
+
+// 挂载flyio
+const baseURL = process.env.API_BASE_URL
+const Flyio = require('flyio/dist/npm/wx')
+const fly = new Flyio()
+fly.config = {
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded'
+  },
+  baseURL: baseURL,
+  parseJson: true
+}
+// 添加响应拦截器，响应拦截器会在then/catch处理之前执行
+fly.interceptors.response.use(
+  (response) => {
+    // 只将请求结果的data字段返回
+    return response.data
+  }, (err) => {
+    // 发生网络错误后会走到这里
+    // todo 后台返回的代码，进行对应的处理
+    if (err.status === 401) {
+      // 需要重新登录
+      wx.redirectTo({url: '/pages/login/main'})
+    }
+    return Promise.reject(err.response.data)
+  }
+)
+// 添加请求拦截器
+fly.interceptors.request.use((request) => {
+  request.headers['Authorization'] = wx.getStorageSync('access_token')
+})
+Vue.prototype.$http = fly
+
 const app = new Vue(App)
 app.$mount()
